e0728b13444bfda08db89e580fd35cdcbea0e9d3
Alex Patel <alexpatel@Alexs-MacBook-Pro.local>
2014-11-25 13:46:03 -0700

    initial fileMenu

--- src/maze/MineGUI.java
+++ src/maze/MineGUI.java
@@ -6,10 +6,14 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 public class MineGUI extends JFrame {
 	private static Mine mine;
 	private ButtonPanel buttonPanel;
+	
 	
 	public MineGUI() {
 		mine = new Mine("MazeLayout.csv");
@@ -20,10 +24,40 @@ public class MineGUI extends JFrame {
 			System.exit(0);
 		}
 		buttonPanel= new ButtonPanel(mine);
+		menuBar();
 		setSize(800, 650);
 		setLayout(new BorderLayout());
 		add(mine, BorderLayout.CENTER);
 		add(buttonPanel, BorderLayout.LINE_END);
+	}
+	
+	public void menuBar() {
+		JMenuBar menu = new JMenuBar();
+		JMenu fileMenu = new JMenu("File");
+		
+		JMenuItem exitItem = new JMenuItem("Exit");
+		exitItem.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		
+		JMenu robotRoutes = new JMenu("Robot routes");
+		JMenuItem robot1 = new JMenuItem("Robot 1");
+		JMenuItem robot2 = new JMenuItem("Robot 2");
+		JMenuItem robot3 = new JMenuItem("Robot 3");
+		JMenuItem robot4 = new JMenuItem("Robot 4");
+		
+		robotRoutes.add(robot1);
+		robotRoutes.add(robot2);
+		robotRoutes.add(robot3);
+		robotRoutes.add(robot4);
+		
+		fileMenu.add(exitItem);
+		fileMenu.add(robotRoutes);
+		menu.add(fileMenu);
+		setJMenuBar(menu);
 	}
 	
 	public static void main(String[] args) throws InterruptedException {
